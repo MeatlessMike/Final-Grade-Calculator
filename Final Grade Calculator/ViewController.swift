e4ab4fe21a84ad1fb35850046ce77eca28f87e91
Akivah <akivahlaviolette@gmail.com>
2022-10-10 09:55:49 -0600

    Initial Commit

---
+++ Final Grade Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final Grade Calculator
+//
+//  Created by Laviolette, Akivah - Student on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
