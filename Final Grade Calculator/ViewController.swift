6db5bab9db3bd410e57610119fc227d825834ed4
Akivah <akivahlaviolette@gmail.com>
2022-10-11 10:02:25 -0600

    sadlk

--- Final Grade Calculator/ViewController.swift
+++ Final Grade Calculator/ViewController.swift
@@ -8,7 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var currentGradeTextField: UITextField!
+    
+    @IBOutlet weak var desiredGradeTextField: UITextField!
+    
+    @IBOutlet weak var finalWorthTextField: UITextField!
+    
+    @IBAction func gradeNeededButton(_ sender: Any) {
+       let currentGrade = Double(currentGradeTextField.text!)
+        
+        let desiredGrade = Double(desiredGradeTextField.text!)
+        
+        let finalWorthPercentage = Double(finalWorthTextField.text!)
+        
+        let oneHundredPercent = 100.0
+        
+        let percentageOfGrade = Double(oneHundredPercent - finalWorthPercentage!)/100.0
+        
+        let gradeNeededOnFinal = (desiredGrade! - (percentageOfGrade * currentGrade!))/finalWorthPercentage!
+        
+        gradeNeededLabel.text = "You Need \(gradeNeededOnFinal * 100.0)%"
+        
+    }
+    
+    @IBOutlet weak var gradeNeededLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
